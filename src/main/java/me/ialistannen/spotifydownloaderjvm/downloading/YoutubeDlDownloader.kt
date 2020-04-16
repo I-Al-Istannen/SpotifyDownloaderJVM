f5dbef9534bcc744c25b75420b605a41bcebfe26
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-04-16 16:22:17 +0200

    Fix output paths with spaces...
    Sadly the underlying Youtube-DL library doesn't support it, so I
    had to be a bit more hacky.

--- src/main/java/me/ialistannen/spotifydownloaderjvm/downloading/YoutubeDlDownloader.kt
+++ src/main/java/me/ialistannen/spotifydownloaderjvm/downloading/YoutubeDlDownloader.kt
@@ -1,9 +1,14 @@
 package me.ialistannen.spotifydownloaderjvm.downloading
 
-import com.sapher.youtubedl.YoutubeDL
+import com.sapher.youtubedl.DownloadProgressCallback
 import com.sapher.youtubedl.YoutubeDLException
 import com.sapher.youtubedl.YoutubeDLRequest
+import com.sapher.youtubedl.YoutubeDLResponse
+import com.sapher.youtubedl.utils.StreamGobbler
+import com.sapher.youtubedl.utils.StreamProcessExtractor
 import io.reactivex.Observable
+import java.io.File
+import java.io.IOException
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -15,7 +20,7 @@ class YoutubeDlDownloader(private val ffmpegDirectory: Path) : Downloader {
 
     override fun download(url: String, path: Path): Observable<Double> {
         return Observable.create {
-            val dlRequest = YoutubeDLRequest(url).apply {
+            val dlRequest = SpaceAwareYoutubeDlRequest(url).apply {
                 setOption("extract-audio")
                 setOption("audio-format", "mp3")
                 setOption("output", makeOutputPath(path))
@@ -24,11 +29,11 @@ class YoutubeDlDownloader(private val ffmpegDirectory: Path) : Downloader {
             }
 
             try {
-                val dlResponse = YoutubeDL.execute(dlRequest) { progress, _ ->
+                val dlResponse = execute(dlRequest, DownloadProgressCallback { progress, _ ->
                     if (!it.isDisposed) {
                         it.onNext(progress.toDouble() / 100)
                     }
-                }
+                })
 
                 if (dlResponse.exitCode != 0) {
                     it.onError(YoutubeDLException("Non-zero exit code: '${dlResponse.exitCode}'"))
@@ -46,16 +51,97 @@ class YoutubeDlDownloader(private val ffmpegDirectory: Path) : Downloader {
     }
 
     private fun getCurrentDirectory() =
-            Paths.get(javaClass.protectionDomain.codeSource.location.toURI())
-                    .parent
-                    .toAbsolutePath()
-                    .toString()
+        Paths.get(javaClass.protectionDomain.codeSource.location.toURI())
+            .parent
+            .toAbsolutePath()
+            .toString()
 
     private fun makeOutputPath(path: Path): String {
         val pathString = path.toAbsolutePath().toString()
-                .replace(".mp3", "")
+            .replace(".mp3", "")
 
         return "$pathString.%(ext)s"
+    }
+
+
+    private class SpaceAwareYoutubeDlRequest(url: String) : YoutubeDLRequest(url) {
+
+        fun buildNicerOptions(): List<String> {
+            val options = option.entries
+                .flatMap {
+                    val res = mutableListOf(
+                        "--" + it.key
+                    )
+                    if (it.value != null) {
+                        res.add(it.value)
+                    }
+                    res
+                }
+                .toMutableList()
+            options.add(0, url)
+
+            return options
+        }
+    }
+
+    @Throws(YoutubeDLException::class)
+    private fun execute(
+        request: SpaceAwareYoutubeDlRequest,
+        callback: DownloadProgressCallback?
+    ): YoutubeDLResponse {
+        val command = listOf("youtube-dl") + request.buildNicerOptions()
+        val directory = request.directory
+        val options = request.option
+
+        val outBuffer = StringBuffer()
+        val errBuffer = StringBuffer()
+        val startTime = System.nanoTime()
+
+        val processBuilder = ProcessBuilder(command)
+        if (directory != null) {
+            processBuilder.directory(File(directory))
+        }
+
+        val process: Process
+        process = try {
+            processBuilder.start()
+        } catch (e: IOException) {
+            throw YoutubeDLException(e)
+        }
+
+        process.inputStream.use { input ->
+            process.errorStream.use { error ->
+                val stdOutProcessor = StreamProcessExtractor(outBuffer, input, callback)
+                val stdErrProcessor = StreamGobbler(errBuffer, error)
+
+                val exitCode: Int
+                exitCode = try {
+                    stdOutProcessor.join()
+                    stdErrProcessor.join()
+                    process.waitFor()
+                } catch (e: InterruptedException) {
+                    throw YoutubeDLException(e)
+                }
+
+                val out = outBuffer.toString()
+                val err = errBuffer.toString()
+
+                return if (exitCode > 0) {
+                    throw YoutubeDLException(err)
+                } else {
+                    val elapsedTime = ((System.nanoTime() - startTime) / 1000000L).toInt()
+                    YoutubeDLResponse(
+                        command.joinToString(" "),
+                        options,
+                        directory,
+                        exitCode,
+                        elapsedTime,
+                        out,
+                        err
+                    )
+                }
+            }
+        }
     }
 
 }
